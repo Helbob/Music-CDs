1015d2b6d5833ac040c5f1dca486f5318eaaa622
Helbob <millehelbo@outlook.dk>
2023-09-01 13:37:17 +0200

    Update

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", start);
+//document.addEventListener("DOMContentLoaded", start);
 
 const Music = {
   author: "",
@@ -9,14 +9,22 @@ const Music = {
 };
 
 const buttonAdd = document.querySelector("#add");
-const buttonRemove = document.querySelector("#remove");
+const buttonRemove = document.querySelector("#delete");
 const musicList = document.querySelector("#list");
-
 buttonAdd.addEventListener("click", addToList);
+const authorInput = document.querySelector("#author");
+const titleInput = document.querySelector("#title");
+const yearInput = document.querySelector("#year");
 buttonRemove.addEventListener("click", removeFromtList);
 
 function addToList() {
+  const klon = document.querySelector("#newtemp").content.cloneNode(true);
+  klon.querySelector("#display").textContent = authorInput.value;
+  klon.querySelector("#displaytwo").textContent = titleInput.value;
+  klon.querySelector("#displaythree").textContent = yearInput.value;
+  musicList.appendChild(klon);
   // Add to list
+  // Appendend child musicList?
 }
 function removeFromtList() {
   // Remove from list
